ce94fafa9d97ba7338ce115726924d3e114d7131
Jonheston <35046180+Jonheston@users.noreply.github.com>
2018-10-11 12:58:37 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,35 @@ var tbody = d3.select("tbody");
 
 
 data.forEach(function(ufosighting) {
-  console.log(ufosighting);
+  // console.log(ufosighting);
   var row = tbody.append("tr");
 
   Object.entries(ufosighting).forEach(function([key, value]) {
-    console.log(key, value);
+    // console.log(key, value);
     var cell = tbody.append("td");
     cell.text(value);
   });
  });
 
-/////seems too easy. I might comeback and do the bonus challenge if time permits.
+var filter = d3.select("#filter-btn");
+  filter.on("click", function() {
+    d3.event.preventDefault();
+    var inputElement = d3.select(".form-control");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = data.filter(sightings => sightings.datetime === inputValue);
+    console.log(filteredData);
+    
+    filteredData.forEach(function(ufosighting) {
+    // console.log(ufosighting);
+    var row = tbody.append("tr");
+  
+    Object.entries(ufosighting).forEach(function([key, value]) {
+      // console.log(key, value);
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+   });
+  });
+
+  ///Almost there. Right now its appeneding the filtered data to the end of the unfiltered data. I just need to figure out how to clear the data and append to a blank table
